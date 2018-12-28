671e80c2c4416a0c237a8cef8e0d77a9e963b8f0
Victor França <fr4ncacc@gmail.com>
2018-12-28 12:52:38 -0200

    laecrim

--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ const posts = require("./routes/api/posts");
 
 app.use("/api/posts", posts);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/public"));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
